744ee3d3c60b0dc86cb639cf71694c440db991eb
tkmrkt <tkmrkt@gmail.com>
2024-02-15 18:17:27 +0900

    lesson2

--- packages/contract/contracts/MyEpicNFT.sol
+++ packages/contract/contracts/MyEpicNFT.sol
@@ -17,7 +17,7 @@ contract MyEpicNFT is ERC721URIStorage {
     Counters.Counter private _tokenIds;
 
     // NFT トークンの名前とそのシンボルを渡します。
-    constructor() ERC721 ("TanyaNFT", "TANYA") {
+    constructor() ERC721 ("SquareNFT", "SQUARE") {
       console.log("This is my NFT contract.");
     }
 
@@ -34,7 +34,7 @@ contract MyEpicNFT is ERC721URIStorage {
       _safeMint(msg.sender, newItemId);
 
       // NFT データを設定します。
-      _setTokenURI(newItemId, "https://api.npoint.io/7a8f388d13850600da54");
+      _setTokenURI(newItemId, "data:application/json;base64,ewogICJuYW1lIjogIkVwaWNOZnRDcmVhdG9yIiwKICAiZGVzY3JpcHRpb24iOiAiVGhlIGhpZ2hseSBhY2NsYWltZWQgc3F1YXJlIGNvbGxlY3Rpb24iLAogICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJad29nSUhodGJHNXpQU0pvZEhSd09pOHZkM2QzTG5jekxtOXlaeTh5TURBd0wzTjJaeUlLSUNCd2NtVnpaWEoyWlVGemNHVmpkRkpoZEdsdlBTSjRUV2x1V1UxcGJpQnRaV1YwSWdvZ0lIWnBaWGRDYjNnOUlqQWdNQ0F6TlRBZ016VXdJZ28rQ2lBZ1BITjBlV3hsUGdvZ0lDQWdMbUpoYzJVZ2V3b2dJQ0FnSUNCbWFXeHNPaUIzYUdsMFpUc0tJQ0FnSUNBZ1ptOXVkQzFtWVcxcGJIazZJSE5sY21sbU93b2dJQ0FnSUNCbWIyNTBMWE5wZW1VNklERTBjSGc3Q2lBZ0lDQjlDaUFnUEM5emRIbHNaVDRLSUNBOGNtVmpkQ0IzYVdSMGFEMGlNVEF3SlNJZ2FHVnBaMmgwUFNJeE1EQWxJaUJtYVd4c1BTSmliR0ZqYXlJZ0x6NEtJQ0E4ZEdWNGRBb2dJQ0FnZUQwaU5UQWxJZ29nSUNBZ2VUMGlOVEFsSWdvZ0lDQWdZMnhoYzNNOUltSmhjMlVpQ2lBZ0lDQmtiMjFwYm1GdWRDMWlZWE5sYkdsdVpUMGliV2xrWkd4bElnb2dJQ0FnZEdWNGRDMWhibU5vYjNJOUltMXBaR1JzWlNJS0lDQStDaUFnSUNCRmNHbGpUbVowUTNKbFlYUnZjZ29nSUR3dmRHVjRkRDRLUEM5emRtYytDZz09Igp9");
 
       // NFTがいつ誰に作成されたかを確認します。
       console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
